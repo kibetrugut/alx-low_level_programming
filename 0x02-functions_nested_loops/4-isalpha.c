4402816fa9c52d7e99b0b4d52697e76968fc9128
kiberugut <rugutbrian1999@gmail.com>
2023-07-18 05:37:43 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: The character to be checked
- * Return: 1 for alphabetic character or 0 for anything else 
+ *Return: 1 for alphabetic character or 0 for anything else
  */
 int _isalpha(int c)
 {
